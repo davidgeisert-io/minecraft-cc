940718c3d3e6df2754c0706881cab4ba51cb86b3
David Geisert <dvdgeisert@gmail.com>
2023-01-18 12:04:10 -0800

    adding ability to get unstuck

--- mine_roomba/position_manager.lua
+++ mine_roomba/position_manager.lua
@@ -129,12 +129,31 @@ end
 
 function position_manager:go_to(x, y, z, rotation, order)
     moving = true
-
     if not order then
         order = {"y", "z", "x"}
     end
 
+    print(string.format("Moving to %d, %d, %d, %d", x, y, z, rotation))
+    local start_position = self:clone_position()
     while moving do
+        stuck = not self:_try_go_to(x, y, z, order)
+        if stuck then
+            print("I'm Stuck!!")
+            unstuck, order = self:_try_get_unstuck(start_position, {x=x, y=y, z=z}, order)
+            print(string.format("I did%s succeed in freeing myself", unstuck and "" or " not"))
+        end
+        moving = position.x ~= x or position.y ~= y or position.z ~= z
+    end
+
+    local rotation_diff = rotation - position.rotation
+
+    self:rotate(rotation_diff)
+end
+
+function position_manager:_try_go_to(x, y, z, order)
+    moving = true
+    while moving do
+        local start_position = self:clone_position()
         for i,v in ipairs(order) do
             if v == "x" and position.x ~=x then
                 self:go_to_x(x)
@@ -148,12 +167,99 @@ function position_manager:go_to(x, y, z, rotation, order)
                 self:go_to_z(z)
             end            
         end
-        moving = position.x ~= x or position.y ~= y or position.z ~= z
+
+        if self:_compare_position(start_position, position) then
+            return false
+        end
+
+        moving = not self:_compare_position(position, {x=x, y=y, z=z})
     end
 
-    local rotation_diff = rotation - position.rotation
+    return true
+end
 
-    self:rotate(rotation_diff)
+function position_manager:_compare_position(position_a, position_b)
+    return position_a.x == position_b.x and
+        position_a.y == position_b.y and
+        position_a.z == position_b.z
+end
+
+function position_manager:_try_get_unstuck(start_position, target_position, order)
+    local moving_axis = order[1]
+    local test_axis = position_manager:get_facing_axis()
+    if moving_axis == test_axis then
+        if test_axis == "x" then
+            local diff_z = target_position.z - start_position.z
+            local target_degrees = self:get_z_axis_target_rotation(diff_z)
+            self:rotate(target_degrees - position.rotation)
+        else
+            local diff_x = target_position.x - start_position.x
+            local target_degrees = self:get_x_axis_target_rotation(diff_x)
+            self:rotate(target_degrees - position.rotation)
+        end
+        
+        test_axis = self:get_facing_axis()
+    end
+
+    print(string.format("Breaking free on %s axis until I can move on my %s axis", moving_axis, test_axis))
+
+    if moving_axis == "x" then
+        local free = false
+        while not free do
+            local diff_x = target_position.x - start_position.x
+            local target_degrees = self:get_x_axis_target_rotation(diff_x)
+            self:rotate(target_degrees - position.rotation)
+
+            local can_move = self:move_back()
+            if not can_move then
+                print("Well shit! I can't move!")
+                return false, {"y", "z", "x"}
+            end
+
+            local diff_z = target_position.z - start_position.z
+            local target_degrees = self:get_z_axis_target_rotation(diff_z)
+            self:rotate(target_degrees - position.rotation)
+
+            free = self:move_forward()
+        end
+        return true, {"z", "x", "y"}
+    elseif moving_axis == "y" then
+        local free = false
+        local diff_y = start_position.y - target_position.y
+        local direction = self:get_y_direction(diff_y)
+        print(string.format("Diff y: %d", diff_y))
+        print(string.format("Moving %s", direction))
+
+        while not free do
+            local can_move, direction = self:_try_move_any(direction)
+            if not can_move then
+                print("Well shit! I can't move!")
+                return false, {self:get_non_facing_axis(), test_axis, "y"}
+            end
+            free = self:move_forward()
+        end
+        return true, {test_axis, moving_axis, self:get_non_facing_axis()}
+    else
+        local free = false
+        while not free do
+            local diff_z = target_position.z - start_position.z
+            local target_degrees = self:get_z_axis_target_rotation(diff_z)
+            self:rotate(target_degrees - position.rotation)
+
+            local can_move = self:move_back()
+            if not can_move then
+                print("Well shit! I can't move!")
+                return false, {"y", "x", "z"}
+            end
+            
+            local diff_x = target_position.x - start_position.x
+            local target_degrees = self:get_x_axis_target_rotation(diff_x)
+            self:rotate(target_degrees - position.rotation)
+
+            free = self:move_forward()
+        end
+        return true, {"x", "z", "y"}
+    end
 end
 
 function position_manager:go_to_x(destination_x)
@@ -179,7 +285,6 @@ function position_manager:go_to_y(destination_y)
     return true
 end
 
-
 function position_manager:go_to_z(destination_z)
     local diff_z = destination_z - position.z
     local target_degrees = self:get_z_axis_target_rotation(diff_z)
@@ -191,6 +296,18 @@ function position_manager:go_to_z(destination_z)
         end
     end
     return true
+end
+
+function position_manager:get_facing_axis()   
+    return (position.rotation == 0 or position.rotation == 180)
+        and "x"
+        or "z"
+end
+
+function position_manager:get_non_facing_axis()
+    return self:get_facing_axis() == "x"
+        and "z"
+        or "x"
 end
 
 function position_manager:get_z_axis_target_rotation(disposition)
@@ -216,6 +333,21 @@ function position_manager:get_y_direction(disposition)
     return "down"
 end
 
+function position_manager:get_opposite_direction(direction)
+    if direction == "up" then
+        return "down"
+    end
+    if direction == "down" then
+        return "up"
+    end
+    if direction == "forward" then
+        return "back"
+    end
+    if direction == "back" then
+        return "forward"
+    end
+end
+
 function position_manager:clone_position()
     local cloned = {}
     cloned.x = position.x
@@ -223,6 +355,15 @@ function position_manager:clone_position()
     cloned.z = position.z
     cloned.rotation = position.rotation
     return cloned
+end
+
+function position_manager:_try_move_any(direction)
+    if self:move(direction) then
+        return true, direction
+    elseif self:move(self:get_opposite_direction(direction)) then
+        return true, self:get_opposite_direction(direction)
+    end
+    return false, nil
 end
 
 return position_manager
